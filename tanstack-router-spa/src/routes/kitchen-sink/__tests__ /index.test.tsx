4789fc31c9281335456ab77d4c113f2b1ea98742
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-02-23 13:41:31 -0800

    finished tests for now

---
+++ tanstack-router-spa/src/routes/kitchen-sink/__tests__ /index.test.tsx
@@ -0,0 +1,28 @@
+import userEvent from '@testing-library/user-event';
+import {
+  render,
+  screen,
+  waitFor,
+} from '@/utils/testing/reactTestingLibraryUtils';
+import createRoutesStub from '@/utils/testing/createRoutesStub';
+import { KitchenSinkPage } from '..';
+
+describe('KitchenSinkPage', () => {
+  it('Displays an error message if the name is too short', async () => {
+    const user = userEvent.setup();
+    const RoutesStub = createRoutesStub([
+      {
+        path: '/',
+        component: KitchenSinkPage,
+      },
+    ]);
+    render(<RoutesStub />);
+    await waitFor(() => expect(screen.getByText('Submit')).toBeInTheDocument());
+    await user.type(screen.getByLabelText('Name'), 'a');
+    await waitFor(() =>
+      expect(
+        screen.getByText('Name must be between 3 and 10 characters'),
+      ).toBeInTheDocument(),
+    );
+  });
+});
