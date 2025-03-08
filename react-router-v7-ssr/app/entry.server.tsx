2ee7d10a028df5a27776e7713d6f7d39b97baad4
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-03-07 23:55:43 -0500

    fix entry server client

--- react-router-v7-ssr/app/entry.server.tsx
+++ react-router-v7-ssr/app/entry.server.tsx
@@ -2,31 +2,32 @@ import { PassThrough } from 'stream';
 import { createReadableStreamFromReadable } from '@react-router/node';
 import type { EntryContext } from 'react-router';
 import { ServerRouter } from 'react-router';
+import type { RenderToPipeableStreamOptions } from 'react-dom/server';
 import { renderToPipeableStream } from 'react-dom/server';
 import { createInstance } from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import Backend from 'i18next-fs-backend/cjs';
+import { isbot } from 'isbot';
 import server from '../mocks/server';
 import i18next from './i18n/i18next.server';
 import i18n from './i18n/i18nConfig';
 import 'dotenv/config';
 import serverEnv from './env.server';
 
-const ABORT_DELAY = 5000;
-
 if (serverEnv.VITE_APP_MSW_ENABLED && serverEnv.NODE_ENV === 'development') {
   server.listen();
 }
+export const streamTimeout = 5_000;
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  reactRouterContext: EntryContext,
+  routerContext: EntryContext,
 ) {
   const instance = createInstance();
   const lng = await i18next.getLocale(request);
-  const ns = i18next.getRouteNamespaces(reactRouterContext);
+  const ns = i18next.getRouteNamespaces(routerContext);
 
   await instance
     .use(initReactI18next)
@@ -39,14 +40,23 @@ export default async function handleRequest(
     });
 
   return new Promise((resolve, reject) => {
-    let didError = false;
+    let shellRendered = false;
+    const userAgent = request.headers.get('user-agent');
+
+    // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
+    // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
+    const readyOption: keyof RenderToPipeableStreamOptions =
+      (userAgent && isbot(userAgent)) || routerContext.isSpaMode
+        ? 'onAllReady'
+        : 'onShellReady';
 
     const { pipe, abort } = renderToPipeableStream(
       <I18nextProvider i18n={instance}>
-        <ServerRouter context={reactRouterContext} url={request.url} />
+        <ServerRouter context={routerContext} url={request.url} />
       </I18nextProvider>,
       {
-        onShellReady: () => {
+        [readyOption]() {
+          shellRendered = true;
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
@@ -55,7 +65,7 @@ export default async function handleRequest(
           resolve(
             new Response(stream, {
               headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
+              status: responseStatusCode,
             }),
           );
 
@@ -65,13 +75,19 @@ export default async function handleRequest(
           reject(error);
         },
         onError(error: unknown) {
-          didError = true;
-
-          console.error(error);
+          responseStatusCode = 500;
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error);
+          }
         },
       },
     );
 
-    setTimeout(abort, ABORT_DELAY);
+    // Abort the rendering stream after the `streamTimeout` so it has time to
+    // flush down the rejected boundaries
+    setTimeout(abort, streamTimeout + 1000);
   });
 }
