042343d43ad46e563dc0f187a7ae76166899043c
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-03-07 23:04:45 -0500

    another bump

--- react-router-v7-spa/react-router.config.ts
+++ react-router-v7-spa/react-router.config.ts
@@ -1,5 +1,15 @@
 import type { Config } from '@react-router/dev/config';
 
+declare module 'react-router' {
+  interface Future {
+    unstable_middleware: true; // 👈 Enable middleware types
+  }
+}
+
 export default {
   ssr: false,
+  future: {
+    unstable_middleware: true,
+    unstable_splitRouteModules: true,
+  },
 } satisfies Config;
