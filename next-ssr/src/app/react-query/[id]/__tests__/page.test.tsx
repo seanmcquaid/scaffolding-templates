7c020bbd9ac70f365736f4c00ac582a9e09ebadd
seanmcquaid <sean.m.mcquaid@gmail.com>
2025-03-21 12:19:03 -0400

    more tests

---
+++ next-ssr/src/app/react-query/[id]/__tests__/page.test.tsx
@@ -0,0 +1,31 @@
+import { useParams } from 'next/navigation';
+import type { MockedFunction } from 'vitest';
+import ReactQueryPostPage from '../page';
+import {
+  render,
+  screen,
+  waitFor,
+} from '@/utils/testing/reactTestingLibraryUtils';
+
+vi.mock('next/navigation', async () => {
+  return {
+    ...(await vi.importActual('next-router-mock')),
+    useParams: vi.fn(),
+  };
+});
+
+const mockUseParams = useParams as MockedFunction<typeof useParams>;
+
+describe('ReactQueryPostPage', () => {
+  it('should render successfully', async () => {
+    mockUseParams.mockReturnValue({ id: '1' });
+    render(<ReactQueryPostPage />);
+    await waitFor(async () =>
+      expect(
+        await screen.findByRole('heading', {
+          level: 1,
+        }),
+      ).toBeInTheDocument(),
+    );
+  });
+});
---
+++ next-ssr/src/app/react-query/__tests__/page.test.tsx
@@ -0,0 +1,21 @@
+import userEvent from '@testing-library/user-event';
+import ReactQueryPage from '../page';
+import {
+  render,
+  screen,
+  waitFor,
+} from '@/utils/testing/reactTestingLibraryUtils';
+
+describe('ReactQueryPage', () => {
+  it('Displays a toast when a post is deleted succesfully', async () => {
+    const user = userEvent.setup();
+    render(<ReactQueryPage />);
+    const deleteButtons = await screen.findAllByText('ReactQueryPage.delete');
+    const firstDeleteButton = deleteButtons[0];
+
+    await user.click(firstDeleteButton);
+    await waitFor(() =>
+      expect(screen.getByText('I got deleted')).toBeInTheDocument(),
+    );
+  });
+});
