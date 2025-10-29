80cc09e71a43cc4003d6ae1fc5e4545965aa2254
Sean McQuaid <sean.mcquaid@cfacorp.com>
2025-10-29 11:06:01 -0400

    compiler

--- templates/next-ssr/next.config.mjs
+++ templates/next-ssr/next.config.mjs
@@ -1,14 +1,6 @@
-import withSvgr from 'next-plugin-svgr';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withSvgr({
-  experimental: {
-    reactCompiler: true,
-  },
-  svgrOptions: {
-    icon: true,
-    titleProp: true,
-  },
-});
+const nextConfig = {
+  reactCompiler: true,
+};
 
 export default nextConfig;
