897a75c535638518f5a06ba27b62bc98577689bb
Sean McQuaid <sean.mcquaid@cfacorp.com>
2026-02-05 13:11:02 -0500

    push up ts template fix

--- templates/typescript-library/tsdown.config.ts
+++ templates/typescript-library/tsdown.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   clean: true,
+  dts: true,
   entry: ['src/**/*', '!src/**/*.test.ts', '!src/**/*.test.tsx'],
+  format: ['esm', 'cjs'],
   unbundle: true,
-  platform: 'neutral',
 });
