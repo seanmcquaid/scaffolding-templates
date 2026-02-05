50e78603268fe2a598e8e2566a7b567ff17dc984
Sean McQuaid <sean.mcquaid@cfacorp.com>
2026-02-05 13:32:05 -0500

    instanbul

--- templates/typescript-library/vitest.config.ts
+++ templates/typescript-library/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['lcov'],
+    },
     pool: 'threads',
     environment: 'happy-dom',
     globals: true,
