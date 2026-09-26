dbac7ba04a958d25213f315f7bfe4d01239df1e2
seanmcquaid <sean.m.mcquaid@gmail.com>
2026-09-25 22:06:40 -0400

    test: disambiguate real API post locator

--- templates/react-router/spa/playwright/e2e/real-api.spec.ts
+++ templates/react-router/spa/playwright/e2e/real-api.spec.ts
@@ -2,6 +2,6 @@ import test, { expect } from '@playwright/test';
 
 test('Tests the real API in the browser', async ({ page }) => {
   await page.goto('http://localhost:3000/kitchen-sink');
-  const post = page.getByText('sunt');
+  const post = page.getByText('sunt aut facere');
   await expect(post).toBeVisible();
 });
